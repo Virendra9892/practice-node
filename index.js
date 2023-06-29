fb2580f927c54fa406017ec8dbfdfdc363145c13
virendrapathak <virendrapathak@nimapinfotech.com>
2023-06-29 19:20:16 +0530

    practice 29_june

---
+++ index.js
@@ -0,0 +1,53 @@
+// const fs = require("fs");
+// let a= fs.readFile("./file/input.txt","utf-8",(err,data)=>{
+//     if(err){
+//         console.log(err)
+//     }
+//     else{
+//         console.log(data)
+//     }
+// })
+// console.log("cdc = ",a);                              
+// console.log(12345)
+
+
+// let obj = {
+//     name: 'm',
+//     age: 2
+// }
+
+// // global.Object = {
+// //     keys: () => {
+
+// //     }
+// // }
+
+// let arr = [1,2,3]
+
+// let brr = 'cdsdasdas'
+// // console.log(Array.from(brr))
+
+// let a = [1,2,4,5]
+
+// console.log(a.length)
+
+// for(i = 0; i < a.length; i++){
+//     console.log(a[i])
+// }
+
+// // console.log(a)
+
+// function a(){
+
+// }
+// func()
+
+// var func = function(){
+//     console.log("hii")
+// }
+// func()
+// function func(){
+//     console.log("second")
+// }
+// func()
+
